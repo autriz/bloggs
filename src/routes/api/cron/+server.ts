0b8d7c3a32f856e7082578252b1f65206a5928b1
autriz <explayinformer8519@gmail.com>
2024-03-31 02:44:09 +0400

    fixed env import

--- src/routes/api/cron/+server.ts
+++ src/routes/api/cron/+server.ts
@@ -1,13 +1,11 @@
 import { error } from "@sveltejs/kit";
 import { auth } from "$lib/server/lucia.js";
+import { CRON_SECRET } from "$env/static/private";
 
 export async function GET(req) {
 	const authHeader = req.request.headers.get("authorization");
 
-	if (
-		!process.env.CRON_SECRET ||
-		authHeader !== `Bearer ${process.env.CRON_SECRET}`
-	)
+	if (!CRON_SECRET || authHeader !== `Bearer ${CRON_SECRET}`)
 		return error(401);
 
 	auth.deleteExpiredSessions();
