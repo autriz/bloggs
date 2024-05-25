549b470b315d64453af18eca32068e2cd32642ac
autriz <explayinformer8519@gmail.com>
2024-05-25 22:16:29 +0400

    Update +page.svelte

--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -9,16 +9,18 @@
 	export let data;
 </script>
 
-<div>
-	<div>
-		<h3>О себе</h3>
-		<p>
-			{data.user.aboutMe ?? ""}
-		</p>
+<div class="mt-4 flex flex-col gap-3 cursor-default">
+	<div class="">
+		<h3 class="text-primary-foreground/80 text-2xl border-b border-primary w-fit select-none">О себе</h3>
+		{#if data.user.aboutMe}
+			<p class="mt-3 ml-3">{data.user.aboutMe}</p>
+		{:else}
+			<p class="mt-3 ml-3 text-primary-foreground/80">Пустота...</p>
+		{/if}
 	</div>
 	<div>
-		<h3>Создан</h3>
-		<p>
+		<h3 class="text-primary-foreground/80 text-2xl border-b border-primary w-fit select-none">Аккаунт создан</h3>
+		<p class="mt-3 ml-3">
 			{dayjs(data.user.createdAt).fromNow()}
 		</p>
 	</div>
